f605ce992e71a6bc7805daeb01f3dedd8e285836
Steven D'Anna <steven.danna@gmail.com>
2013-08-10 20:59:34 -0500

    Wiring up the back end

--- sdnugmeetinghtmlfragmentgenerator/Controllers/HomeController.cs
+++ sdnugmeetinghtmlfragmentgenerator/Controllers/HomeController.cs
@@ -20,13 +20,22 @@ namespace sdnugmeetinghtmlfragmentgenerator.Controllers
         [HttpPost]
         public ActionResult Index(HtmlFragmentCreateVM vm)
         {
-            vm.HtmlFragment = HTMLEVENTFORMATTER;
+            vm.HtmlFragment = GenerateEvent(vm);
             return View(vm);
         }
 
         public string GenerateEvent(HtmlFragmentCreateVM vm)
         {
-            return "";
+            return string.Format(HTMLEVENTFORMATTER,
+                                 vm.MeetingDate.ToString("MMMM"),
+                                 vm.MeetingDate.ToShortDateString(),
+                                 vm.SpeakerName,
+                                 vm.SpeakerBio,
+                                 vm.MeetingTopic,
+                                 vm.MeetingTopicSummary,
+                                 CENTENARY_LOCATION,
+                                 vm.RSVPFacebookLink,
+                                 DateTime.Now.ToShortDateString());
         }
 
         public DateTime Get3rdWednesdayOfCurrentMonth()
@@ -42,7 +51,7 @@ namespace sdnugmeetinghtmlfragmentgenerator.Controllers
 
         private const string HTMLEVENTFORMATTER = @"
             <div class=""post speaker-event"">
-		        <h1 class=""title"">{0} - Meeting</h1>
+		        <h1 class=""title"">{0} Meeting</h1>
 		        <div class=""entry"">
 			        <p><b>When:</b> {1} 6:00 PM - 8:00 PM</p>
 			        <p><b>Speaker:</b> {2}</p>
@@ -52,12 +61,15 @@ namespace sdnugmeetinghtmlfragmentgenerator.Controllers
 			        <p>
 				        {5}
 			        </p>
-			        <p><b>Location:</b> <a href=""http://www.cohabitat.us/shreveport"">CoHabitat</a> - 610 Commerce St., Shreveport, LA <a href=""map.asp"" class=""more"">map</a></p>
-			        <p><b>RSVP:</b> <a href=""{6}"">Click here</a><p>
+			        {6}
+			        <p><b>RSVP:</b> <a href=""{7}"">Click here</a><p>
 		        </div>
 		        <div class=""meta""> 
-			        <p class=""links""> posted{7}</p> 
+			        <p class=""links""> posted {8} by Steven D'Anna</p> 
 		        </div> 
-	        </div>";
+	        </div><hr />";
+
+        private const string COHAB_LOCATION = @"<p><b>Location:</b> <a href=""http://www.cohabitat.us/shreveport"">CoHabitat</a> - 610 Commerce St., Shreveport, LA  71101<a href=""map.asp"" class=""more"">map</a></p>";
+        private const string CENTENARY_LOCATION = @"<p><b>Location:</b> <a href=""http://www.centenary.edu/campusmap"">Centenary College</a> Jackson Hall, Room 211 - 2911 Centenary Boulevard, Shreveport, LA  71104</p>";
     }
 }
