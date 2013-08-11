a751f8ba99ff28785fa73613909797894b51fbde
Steven D'Anna <steven.danna@gmail.com>
2013-08-10 19:45:35 -0500

    Getting basic flow down

--- sdnugmeetinghtmlfragmentgenerator/Controllers/HomeController.cs
+++ sdnugmeetinghtmlfragmentgenerator/Controllers/HomeController.cs
@@ -3,30 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using sdnugmeetinghtmlfragmentgenerator.Models;
 
 namespace sdnugmeetinghtmlfragmentgenerator.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
+            var vm = new HtmlFragmentCreateVM();
+            return View(vm);
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(HtmlFragmentCreateVM vm)
         {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
+            vm.HtmlFragment = HTMLEVENTFORMATTER;
+            return View(vm);
         }
 
-        public ActionResult Contact()
+        public string GenerateEvent(HtmlFragmentCreateVM vm)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return "";
         }
+
+        private const string HTMLEVENTFORMATTER = @"
+            <div class=""post speaker-event"">
+		        <h1 class=""title"">{0} - Meeting</h1>
+		        <div class=""entry"">
+			        <p><b>When:</b> {1} 6:00 PM - 8:00 PM</p>
+			        <p><b>Speaker:</b> {2}</p>
+			        <p><b>Speaker Bio:</b> {3}</p>
+			        <p><b>Topic:</b> {4}</p>
+			        <p><b>Summary:</b></p>
+			        <p>
+				        {5}
+			        </p>
+			        <p><b>Location:</b> <a href=""http://www.cohabitat.us/shreveport"">CoHabitat</a> - 610 Commerce St., Shreveport, LA <a href=""map.asp"" class=""more"">map</a></p>
+			        <p><b>RSVP:</b> <a href=""{6}"">Click here</a><p>
+		        </div>
+		        <div class=""meta""> 
+			        <p class=""links""> posted{7}</p> 
+		        </div> 
+	        </div>";
     }
 }
---
+++ sdnugmeetinghtmlfragmentgenerator/Models/HtmlFragmentCreateVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace sdnugmeetinghtmlfragmentgenerator.Models
+{
+    public class HtmlFragmentCreateVM
+    {
+        public string HtmlFragment { get; set; }
+
+        public DateTime MeetingDate { get; set; }
+        public string SpeakerName { get; set; }
+        public string SpeakerBio { get; set; }
+        public string MeetingTopic { get; set; }
+        public string MeetingTopicSummary { get; set; }
+        public string RSVPFacebookLink { get; set; }
+    }
+}
